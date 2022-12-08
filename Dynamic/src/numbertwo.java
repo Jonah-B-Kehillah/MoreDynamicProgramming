91a1e11110c95ed819834ff555573815a162c0ed
MyBeets <ikeller2023@kehillahstudent.org>
2022-12-07 18:40:03 -0800

    things are more complicated than I thought. currently screaming crying and rolling on the floor (staring blankly at my computer)

--- Dynamic/src/numbertwo.java
+++ Dynamic/src/numbertwo.java
@@ -1,5 +1,6 @@
 import java.util.Stack;
 public class numbertwo {
+    public int n = 3;
 	/*
 	Given an n x n grid with non-negative entries x_{i, j} for i, j \in [n],
 	find the minimum-cost path from the bottom left of the grid to the top
@@ -8,31 +9,60 @@ public class numbertwo {
 	//just use djisktra lol
 
     public static void main(String args[]) {
-        int n = 1;
-        int[][] grid = new int[n][n];
+
+        //int[][] grid = new int[n][n];
+        int[][] grid = new int[][]{{1,1,1},{1,1,1},{1,1,1}};
         int[] bestroute = bestroute(grid);
 	}
 
     public static int[] bestroute(int[][] grid){
         int n = grid.length;
         int[][] gridval = new int[n][n];
-        Stack<int[]> stack = new Stack<int[]>();
 
         //initial condition
         int r=0;
         int c=n-1;
-        int[] idx = {r,c};
+        gridval[r][c] = grid[r][c];
 
-        stack.push(idx);
+        Stack<int[]> adj = getadj(new int[]{r,c});
 
+        while(!adj.empty()){
+            int[] idx = adj.pop();
+            if(gridval[idx[0]][idx[1]]==0) gridval[idx[0]][idx[1]] = getvalue(idx);
+        }
 
-
-        pushval(r,c);
-
+        for(int i = 0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(" "+gridval[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
-    public static int[][] pushval(int[] idx){
-        int[][] adj;
+    public static Stack getadj(int[] idx){
+        Stack<int[]> adj = new Stack<int[]>();
+        if(idx[0] == 0){
+            if(idx[1] >= n-1) return null;
+            adj.push(new int[] {idx[0],idx[1]+1});
+        }
+        else if(idx[1] >= n-1) adj.push(new int[] {idx[0]-1,idx[1]});
+        else{
+            adj.push(new int[] {idx[0],idx[1]+1});
+            adj.push(new int[] {idx[0]-1,idx[1]});
+        }
+        return adj;
+    }
+    public static int getvalue(int[] idx){
+        if(idx[0] == n){
+            if(idx[1] >= n-1) return null;
+            adj.push(new int[] {idx[0],idx[1]+1});
+        }
+        else if(idx[1] >= n-1) adj.push(new int[] {idx[0]-1,idx[1]});
+        else{
+            adj.push(new int[] {idx[0],idx[1]+1});
+            adj.push(new int[] {idx[0]-1,idx[1]});
+        }
+        return adj;
     }
 
 }
