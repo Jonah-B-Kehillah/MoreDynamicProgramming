69a7110ab2ce88f8d2b4819c75c72fb8dea3e850
MyBeets <ikeller2023@kehillahstudent.org>
2022-12-07 18:07:23 -0800

    working on number two. should be done soon, I'm just messing around with using an array versus a stack

--- Dynamic/src/numbertwo.java
+++ Dynamic/src/numbertwo.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 public class numbertwo {
 	/*
 	Given an n x n grid with non-negative entries x_{i, j} for i, j \in [n],
@@ -5,5 +6,33 @@ public class numbertwo {
 	right of the grid, where on each step of the path we either step up or to the right.
 	 */
 	//just use djisktra lol
-	
+
+    public static void main(String args[]) {
+        int n = 1;
+        int[][] grid = new int[n][n];
+        int[] bestroute = bestroute(grid);
+	}
+
+    public static int[] bestroute(int[][] grid){
+        int n = grid.length;
+        int[][] gridval = new int[n][n];
+        Stack<int[]> stack = new Stack<int[]>();
+
+        //initial condition
+        int r=0;
+        int c=n-1;
+        int[] idx = {r,c};
+
+        stack.push(idx);
+
+
+
+        pushval(r,c);
+
+
+    }
+    public static int[][] pushval(int[] idx){
+        int[][] adj;
+    }
+
 }
